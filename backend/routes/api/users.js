e4ade73929d0e60f59ad2ed5b322b7acde516ef4
Minh Tran <minhttran93@gmail.com>
2023-06-08 16:28:04 -0700

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,10 +1,38 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 
+// express-validator & handleValidationErrors imports
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+// auth
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
+// router initialized
 const router = express.Router();
+
+// validateSignup middleware composed of the express-validator 'check' and handleValidationErrors function
+const validateSignup = [
+  //checks to see if req.body.email exists and is an email,
+  check("email")
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage("Please provide a valid email."),
+  //req.body.username is a minimum length of 4 and is not an email
+  check("username")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage("Please provide a username with at least 4 characters."),
+  check("username").not().isEmail().withMessage("Username cannot be an email."),
+  //req.body.password is not empty and has a minimum length of 6
+  check("password")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage("Password must be 6 characters or more."),
+  //If at least one of the req.body values fail the check, an error will be returned as the response.
+  handleValidationErrors,
+];
 
 /**USER SIGNUP API ROUTE - post
  * 1. deconstruct the req.body
@@ -14,7 +42,7 @@ const router = express.Router();
  * 5. use setTokenCookie to log in the user by creating a JWT cookie with the user's non-sensitive information as its payload
  * 6. send a JSON response containing user's non-sensitive info
  */
-router.post("/", async (req, res) => {
+router.post("/", validateSignup, async (req, res) => {
   const { email, password, username } = req.body;
   const hashedPassword = bcrypt.hashSync(password);
   const user = await User.create({ email, username, hashedPassword });
