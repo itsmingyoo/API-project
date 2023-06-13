9455b8103a47750f451606827b5ae625894aa4dc
Minh Tran <minhttran93@gmail.com>
2023-06-13 16:26:00 -0700

    Add handleValidationErrors for CreateSpot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,7 +2,39 @@ const express = require("express");
 const { Op } = require("sequelize");
 const { Spot, User, SpotImage, sequelize, Review } = require("../../db/models");
 const { getCurrentUser } = require("../../utils/auth");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 const router = express.Router();
+const validateSpot = [
+  check("address")
+    .exists({ checkFalsy: true })
+    .withMessage("Street address is required"),
+  check("city").exists({ checkFalsy: true }).withMessage("City is required"),
+  check("state").exists({ checkFalsy: true }).withMessage("State is required"),
+  check("country")
+    .exists({ checkFalsy: true })
+    .withMessage("Country is required"),
+  check("lat")
+    .exists({ checkFalsy: true })
+    .isFloat()
+    .withMessage("Latitude is not valid"),
+  check("lng")
+    .exists({ checkFalsy: true })
+    .isFloat()
+    .withMessage("Longitude is not valid"),
+  check("name")
+    .exists({ checkFalsy: true })
+    .isLength({ max: 50 })
+    .withMessage("Name must be less than 50 characters"),
+  check("description")
+    .exists({ checkFalsy: true })
+    .withMessage("Description is required"),
+  check("price")
+    .exists({ checkFalsy: true })
+    .isNumeric()
+    .withMessage("Price per day is required"),
+  handleValidationErrors,
+];
 
 // 1. get all spots
 router.get("/", async (req, res) => {
@@ -63,7 +95,7 @@ router.get("/", async (req, res) => {
 // });
 
 // 3. Create a Spot
-router.post("/", getCurrentUser, async (req, res) => {
+router.post("/", getCurrentUser, validateSpot, async (req, res) => {
   const { address, city, state, country, lat, lng, name, description, price } =
     req.body;
   if (
