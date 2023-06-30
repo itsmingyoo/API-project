325d3a889ac0f1039cc25bd5be4532cb9598c6b1
Minh Tran <minhttran93@gmail.com>
2023-06-30 11:21:08 -0700

    p4 modals

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -4,6 +4,7 @@ import { csrfFetch } from "./csrf";
 const SET_USER = "session/setUser";
 const REMOVE_USER = "session/removeUser";
 
+// add/set user action
 const setUser = (user) => {
   return {
     type: SET_USER,
@@ -11,12 +12,14 @@ const setUser = (user) => {
   };
 };
 
+// remove user action
 const removeUser = () => {
   return {
     type: REMOVE_USER,
   };
 };
 
+// login thunk
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
@@ -31,6 +34,34 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+// signup thunk
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+// logout thunk
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session", {
+    method: "DELETE",
+  });
+  dispatch(removeUser());
+  return response;
+};
+
+// restoresUser thunk
 export const restoreUser = () => async (dispatch) => {
   const response = await csrfFetch("/api/session");
   const data = await response.json();
