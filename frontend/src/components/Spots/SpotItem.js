1cf267baa8c64d4de3d2657006e33f6176e4c084
Minh Tran <minhttran93@gmail.com>
2023-07-01 19:24:11 -0700

    add favicon - important note: favicons should be in the frontend/public folder bc the html file cannot read outside that unless it is a direct url link

--- frontend/src/components/Spots/SpotItem.js
+++ frontend/src/components/Spots/SpotItem.js
@@ -8,10 +8,10 @@ function SpotItem({ spot }) {
       <img src="https://cdn.vox-cdn.com/thumbor/lFQ3pGOjzP1XQ5puQms14ad7Wv8=/0x0:1400x788/920x613/filters:focal(588x282:812x506):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/70412073/0377c76083423a1414e4001161e0cdffb0b36e1f_760x400.0.png" />
       <div>{spot.name}</div> {/* this is just temp */}
       <div id="spot__item-info">
-        <div>{spot.city}</div>
+        <div>{spot.city}, </div>
         <div>{spot.state}</div>
       </div>
-      <div id="spot__item-price">{spot.price}</div>
+      <div id="spot__item-price">${spot.price}</div>
     </>
   );
 }
