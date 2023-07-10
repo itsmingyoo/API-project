02862144b32507fa4a60b3cda60411cc6ac7f38f
Minh Tran <minhttran93@gmail.com>
2023-07-09 23:35:57 -0700

    fix major error with toFixed

--- frontend/src/components/Spots/SpotItem.js
+++ frontend/src/components/Spots/SpotItem.js
@@ -10,14 +10,14 @@ function SpotItem({ spot }) {
           {spot.city}, {spot.state}
         </div>
         {spot.avgRating ? (
-          <div className="big-bold">★ {spot.avgRating.toFixed(1)}</div>
+          <div className="big-bold">★ {Number(spot.avgRating).toFixed(1)}</div>
         ) : (
           <div className="big-bold">★ New</div>
         )}
       </div>
       <div id="spot__item-price-night">
         <span id="spot__item-price" className="big-bold">
-          ${`${spot.price.toFixed(2)}  `}
+          ${`${Number(spot.price).toFixed(2)}  `}
           &nbsp;
         </span>
         <label>night</label>
