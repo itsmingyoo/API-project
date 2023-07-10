d8770009a4faeac68e205f54bc163f7d7191e9a7
Minh Tran <minhttran93@gmail.com>
2023-07-09 22:49:43 -0700

    fix border-radius on manage spots

--- frontend/src/components/Spots/SpotItem.js
+++ frontend/src/components/Spots/SpotItem.js
@@ -10,14 +10,14 @@ function SpotItem({ spot }) {
           {spot.city}, {spot.state}
         </div>
         {spot.avgRating ? (
-          <div className="big-bold">★ {spot.avgRating}</div>
+          <div className="big-bold">★ {spot.avgRating.toFixed(1)}</div>
         ) : (
           <div className="big-bold">★ New</div>
         )}
       </div>
       <div id="spot__item-price-night">
         <span id="spot__item-price" className="big-bold">
-          ${`${spot.price}  `}
+          ${`${spot.price.toFixed(2)}  `}
           &nbsp;
         </span>
         <label>night</label>
